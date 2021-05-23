f390883f0ab8c26b11065be71135e8e1df8388ee
Frederick Jodozi <fredfred776@gmail.com>
2021-05-22 22:35:12 -0400

    feature/javascript

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log("hello");
+const profileEditButton = document.querySelector(".profile__edit-button");
+
+const modalEL = document.querySelector(".modal");
+const modalCloseButton = document.querySelector(".modal__close-button");
+
+const modalInputName = document.querySelector(".js-modal__input-name");
+const modalInputProfession = document.querySelector(".js-modal__input-profession");
+const profileName = document.querySelector(".profile__name")
+const profileProfession = document.querySelector(".profile__profession");
+const saveButton = document.querySelector(".modal__save-button");
+
+profileEditButton.addEventListener("click", function() {
+    modalEL.classList.add("modal__open");
+});
+
+function closeModal () {
+    modalEL.classList.remove("modal__open");  
+}
+
+modalCloseButton.addEventListener("click", function() {
+    closeModal();
+});
+
+modalInputName.value = profileName.textContent;
+modalInputProfession.value = profileProfession.textContent;
+
+saveButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    profileName.textContent = modalInputName.value;
+    profileProfession.textContent = modalInputProfession.value;
+    closeModal();
+});
+
+
+
