6c59da813d0dd85ff04622cd0367afc5dbd2a181
Fred776 <fredfred776@gmail.com>
2021-06-03 12:19:56 -0400

    hotfix/organise js file

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,23 @@
-const profileEditButton = document.querySelector(".profile__edit-button");
-
+// *** Wrappers ***
 const modalEL = document.querySelector(".modal");
-const modalCloseButton = document.querySelector(".modal__close-button");
-
-const modalInputName = document.querySelector(".modal__input_content_name");
-const modalInputProfession = document.querySelector(".modal__input_content_profession");
-const profileName = document.querySelector(".profile__name")
-const profileProfession = document.querySelector(".profile__profession");
 const formModal = document.querySelector(".modal__form");
 
+// *** DOM elements ***
+const profileName = document.querySelector(".profile__name")
+const profileProfession = document.querySelector(".profile__profession");
+
+// *** Buttons ***
+const profileEditButton = document.querySelector(".profile__edit-button");
+const modalCloseButton = document.querySelector(".modal__close-button");
+
+// *** Form Data ***
+const modalInputName = document.querySelector(".modal__input_content_name");
+const modalInputProfession = document.querySelector(".modal__input_content_profession");
+
+// *** Event listeners ***
+profileEditButton.addEventListener("click", openModal);
+modalCloseButton.addEventListener("click", closeModal);
+formModal.addEventListener("submit", saveAndCloseModal);
 
 function openModal () {
     modalEL.classList.add("modal_open");
@@ -20,21 +29,12 @@ function closeModal () {
     modalEL.classList.remove("modal_open");  
 }
 
-profileEditButton.addEventListener("click", openModal);
-
-modalCloseButton.addEventListener("click", closeModal);
-
-
-
 function saveAndCloseModal(event) {
     event.preventDefault();
     profileName.textContent = modalInputName.value;
     profileProfession.textContent = modalInputProfession.value;
     closeModal();
 }
-
-formModal.addEventListener("submit", saveAndCloseModal);
-
 
 /*
 const cardLikeButton = document.querySelectorAll(".card__like-button");
