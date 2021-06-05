4ea6b3e800242eb05df7b7439e9f61251aa0be25
Fred776 <fredfred776@gmail.com>
2021-06-04 20:59:51 -0400

    feature/cardAddButton event listener

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,65 @@
+// *** Wrappers ***
+const editModalEl = document.querySelector(".modal_type_edit");
+const addModalEl = document.querySelector(".modal_type_add");
+const editFormModal = document.querySelector(".modal__form_type_edit");
+const addFormModal = document.querySelector(".modal__form_type_add");
+const placesList = document.querySelector(".places__list");
+
+// *** DOM elements ***
+const profileName = document.querySelector(".profile__name")
+const profileProfession = document.querySelector(".profile__profession");
+
+// *** Buttons ***
+const profileEditButton = document.querySelector(".profile__edit-button");
+const editModalCloseButton = document.querySelector(".modal__close-button_type_edit");
+const cardAddButton = document.querySelector(".profile__add-button")
+const addModalCloseButton = document.querySelector(".modal__close-button_type_add");
+
+// *** Form Data ***
+const modalInputName = document.querySelector(".modal__input_content_name");
+const modalInputProfession = document.querySelector(".modal__input_content_profession");
+
+// *** Event listeners ***
+profileEditButton.addEventListener("click", () => {
+    toggleModal(editModalEl);
+});
+
+editModalCloseButton.addEventListener("click", () => {
+    toggleModal(editModalEl);
+});
+
+cardAddButton.addEventListener("click", () => {
+    toggleModal(addModalEl)
+})
+
+addModalCloseButton.addEventListener("click", () => {
+    toggleModal(addModalEl);
+});
+
+editFormModal.addEventListener("submit", submitEditModal);
+
+// *** Modal functions ***
+function toggleModal(modal) {
+    if(modal.classList.contains("modal_type_edit")) {
+        modalInputName.value = profileName.textContent;
+        modalInputProfession.value = profileProfession.textContent;
+    }
+    modal.classList.toggle("modal_open");
+}
+
+function submitEditModal(event) {
+    event.preventDefault();
+    profileName.textContent = modalInputName.value;
+    profileProfession.textContent = modalInputProfession.value;
+    toggleModal(editModalEl);
+}
+
+function submitAddModal(event) {
+    event.preventDefault();
+
+    toggleModal(addModalEl);
+}
+
 // *** Card array ***
 const initialCards = [ 
     {
@@ -26,46 +88,8 @@ const initialCards = [
     },
 ];
 
-// *** Wrappers ***
-const modalEl = document.querySelector(".modal");
-const formModal = document.querySelector(".modal__form");
-const placesList = document.querySelector(".places__list");
-
-// *** DOM elements ***
-const profileName = document.querySelector(".profile__name")
-const profileProfession = document.querySelector(".profile__profession");
-
-// *** Buttons ***
-const profileEditButton = document.querySelector(".profile__edit-button");
-const modalCloseButton = document.querySelector(".modal__close-button");
-
-// *** Form Data ***
-const modalInputName = document.querySelector(".modal__input_content_name");
-const modalInputProfession = document.querySelector(".modal__input_content_profession");
-
 // *** Templates ***
 const cardTemplate = document.querySelector(".card__template").content;
-
-// *** Event listeners ***
-profileEditButton.addEventListener("click", toggleModal);
-modalCloseButton.addEventListener("click", toggleModal);
-formModal.addEventListener("submit", saveAndCloseModal);
-
-// *** Modal functions ***
-function toggleModal () {
-    if(modalEl.classList.contains("modal_open")) {
-        modalInputName.value = profileName.textContent;
-        modalInputProfession.value = profileProfession.textContent;
-    }
-    modalEl.classList.toggle("modal_open");
-}
-
-function saveAndCloseModal(event) {
-    event.preventDefault();
-    profileName.textContent = modalInputName.value;
-    profileProfession.textContent = modalInputProfession.value;
-    toggleModal();
-}
 
 // *** Card functions ***
 function generateCard (card) {
