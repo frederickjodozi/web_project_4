8534fbd0e2e60f7ed1ce9bbfe483a3b1478418a3
Fred776 <fredfred776@gmail.com>
2021-06-26 17:54:25 -0400

    hotfix/reorganise validate.js

--- scripts/validate.js
+++ scripts/validate.js
@@ -2,21 +2,13 @@ const hideInputError = (formEl, inputEl, settings) => {
     const errorSpan = formEl.querySelector("#" + inputEl.id + "-error");
     errorSpan.textContent = "";
     inputEl.classList.remove(settings.errorClass);
-}
+};
 
 const showInputError = (formEl, inputEl, settings) => {
     const errorSpan = formEl.querySelector("#" + inputEl.id + "-error");
     errorSpan.textContent = inputEl.validationMessage;
     inputEl.classList.add(settings.errorClass);
-}
-
-const checkInputValidity = (formEl, inputEl, settings) => {
-    if(inputEl.validity.valid) {
-        hideInputError(formEl, inputEl, settings)
-    } else {
-        showInputError(formEl, inputEl, settings);
-    }
-}
+};
 
 const hasValidInputs = (inputElements) => 
     inputElements.every((inputEl) => inputEl.validity.valid === true);
@@ -29,7 +21,15 @@ const toggleButton = (inputElements, submitButton, settings) => {
         submitButton.disabled = true;
         submitButton.classList.add(settings.inactiveButtonClass);
     };
-}
+};
+
+const checkInputValidity = (formEl, inputEl, settings) => {
+    if(inputEl.validity.valid) {
+        hideInputError(formEl, inputEl, settings)
+    } else {
+        showInputError(formEl, inputEl, settings);
+    };
+};
 
 const setEventListeners = (formEl, settings) => {
     const inputElements = [...formEl.querySelectorAll(settings.inputSelector)];
@@ -38,9 +38,9 @@ const setEventListeners = (formEl, settings) => {
         inputEl.addEventListener("input", (e) => {
             checkInputValidity(formEl, inputEl, settings);
             toggleButton(inputElements, submitButton, settings);
+        });
     });
-});
-}
+};
 
 const enableValidation = (settings) => {
     const formElements = [...document.querySelectorAll(settings.formSelector)];
@@ -48,7 +48,7 @@ const enableValidation = (settings) => {
         formEl.addEventListener("submit", (e) => e.preventDefault());
         setEventListeners(formEl, settings);
     });
-}
+};
 
 const variableConfig = {
     formSelector: ".modal__form",
@@ -56,6 +56,6 @@ const variableConfig = {
     submitButtonSelector: ".modal__save-button",
     errorClass: "modal__input-error_active",
     inactiveButtonClass: "modal__save-button_inactive"
-}
+};
 
 enableValidation(variableConfig);
