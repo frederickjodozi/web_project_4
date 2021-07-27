ca64596111486ca64e8fceb2f4ef1fd8f81f0473
Fred776 <fredfred776@gmail.com>
2021-07-26 21:25:48 -0500

    create Popup js class

---
+++ components/popup.js
@@ -0,0 +1,31 @@
+class Popup {
+    constructor(popupSelector) {
+        this._popupElement = document.querySelector(`.${popupSelector}`);
+        this._handleEscUp = this._handleEscUp.bind(this);
+    }
+
+    open = () => {
+        this._popupElement.classList.add("modal_open");
+        document.addEventListener("keyup", this._handleEscUp);
+    }
+    
+    close = () => {
+        this._popupElement.classList.remove("modal_open");
+        document.removeEventListener("keyup", this._handleEscUp);
+    }
+
+    _handleEscUp = (evt) => {
+        evt.preventDefault(); 
+        if(evt.key === "Escape") {
+            this.close();
+        }
+    }
+
+    setEventListeners() {
+        this._popupElement.addEventListener("click", (evt) => {
+            if(evt.target.classList.contains('modal') || evt.target.classList.contains('modal__close-button')) {
+                this.close();
+            }
+        });
+    }
+}
--- utils/constants.js
+++ utils/constants.js
@@ -1,31 +1,3 @@
-// *** Open/Close modal functions ***
-const openModal = (modal) => {
-    modal.classList.add("modal_open");
-    modal.addEventListener("click", closeModalByClick);
-    document.addEventListener("keydown", closeModalByEscapeKey);
-}
-
-const closeModal = (modal) => {
-    modal.classList.remove("modal_open");
-    modal.removeEventListener("click", closeModalByClick);
-    document.removeEventListener("keydown", closeModalByEscapeKey);
-}
-
-const closeModalByClick = (e) => {
-    const modalOpen = document.querySelector(".modal_open");
-    if(e.target === modalOpen) {
-        closeModal(modalOpen);
-    };
-}
-
-const closeModalByEscapeKey = (e) => {
-    const modalOpen = document.querySelector(".modal_open");
-    if(e.key === "Escape") {
-        closeModal(modalOpen);
-    };
-}
-
-
 // *** Image Preview Modal ***
 const imagePreviewModalEl = document.querySelector(".modal_type_image-preview");
 const imagePreviewEl = document.querySelector(".modal__image");
