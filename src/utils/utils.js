9b3eadbb22836543233f3ba2c0da1ca39e652d2a
Fred776 <fredfred776@gmail.com>
2021-08-15 23:19:07 -0500

    fix utils.js formatting

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,12 +3,12 @@ import {imagePreviewModalEl, imagePreviewEl, captionPreviewEl} from "./constants
 
 function handleCardClick() {
     this._image.addEventListener("click", (evt) => {
-    evt.preventDefault;
-    const imagePopup = new PopupWithImage(imagePreviewModalEl)
-    imagePopup.open(this._link, this._name);
-    imagePreviewEl.src= this._link;
-    imagePreviewEl.alt= this._name;
-    captionPreviewEl.textContent = this._name;
+        evt.preventDefault;
+        const imagePopup = new PopupWithImage(imagePreviewModalEl)
+        imagePopup.open(this._link, this._name);
+        imagePreviewEl.src= this._link;
+        imagePreviewEl.alt= this._name;
+        captionPreviewEl.textContent = this._name;
     });
 }
 
