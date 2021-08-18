cd0b5d473ab7a2b00fafdd1d2c53b2df4d24c794
Fred776 <fredfred776@gmail.com>
2021-08-17 23:06:52 -0500

    rename user-info.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import {handleCardClick} from "../utils/utils.js";
 import Section from "../components/Section.js";
 import Card from "../components/card.js";
 import PopupWithForm from "../components/popup-with-form.js";
-import UserInfo from "../components/userInfo.js";
+import UserInfo from "../components/user-info.js";
 import FormValidator from "../components/form-validator.js";
 
 import headerImage from "../images/header__logo.svg";
