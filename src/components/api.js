2f04f44e22376acb372cb71497d701847bbda4bc
Fred776 <fredfred776@gmail.com>
2021-09-22 17:34:37 -0500

    create js class Api

---
+++ src/components/api.js
@@ -0,0 +1,18 @@
+export default class Api {
+    constructor(baseUrl, authToken) {
+        this._baseUrl = baseUrl;
+        this._authToken = authToken
+    }
+    
+    getCards() {
+        return fetch(`${this._baseUrl}/cards`, {
+            headers: {
+                authorization: this._authToken
+            }
+        })
+            .then(res => {res.ok ? res.json() : Promise.reject(`Err: ${res.status}`)})
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+}
