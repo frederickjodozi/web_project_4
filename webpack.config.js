e016c7abd76d1d22f2416d0afa2cba53efabcc04
Fred776 <fredfred776@gmail.com>
2021-12-07 14:16:07 -0600

    remove regenerator-runtime from webpack.config entry point to fix errors generated during npm run build

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,9 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: ["regenerator-runtime/runtime.js",{
+  entry: {
     main: './src/pages/index.js'
-  }],
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
